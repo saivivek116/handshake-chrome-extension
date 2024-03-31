fd38374bc988ad6d136bc9408833c0ba9bf5e63d
Sai Kiran <saikirankudumula@gmail.com>
2024-03-31 15:07:22 -0400

    Intial commit

---
+++ content.js
@@ -0,0 +1,50 @@
+function insertBanner() {
+  var bannerHtml = `<div id="my-custom-banner">
+   <img src="${chrome.runtime.getURL(
+     "images/icon16.png"
+   )}" alt="Icon" class="banner-icon">
+        <span><strong>17 of 32 keywords</strong> are present in your resume</span>
+        <div class="progress-bar">
+          <div class="progress"></div>
+        </div>
+        <span class="resume-match">53% resume match</span>
+        <button class="close-btn">×</button>
+      </div>`; // Your banner HTML code here
+  var targetDiv = document.querySelector(
+    "div.style__application-flow-new___JIHOx"
+  );
+
+  if (targetDiv) {
+    targetDiv.insertAdjacentHTML("afterend", bannerHtml);
+    var closeButton = document.querySelector("#my-custom-banner .close-btn");
+    if (closeButton) {
+      closeButton.addEventListener("click", function () {
+        document.querySelector("#my-custom-banner").remove();
+      });
+    }
+  } else {
+    console.log("Target div still not found after DOM changes.");
+  }
+}
+
+// MutationObserver callback to execute when mutations are observed
+var callback = function (mutationsList, observer) {
+  for (var mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      var targetDiv = document.querySelector(
+        "div.style__application-flow-new___JIHOx"
+      );
+      if (targetDiv) {
+        insertBanner();
+        observer.disconnect(); // Stop observing after the target div is found
+        break;
+      }
+    }
+  }
+};
+
+// Create a MutationObserver instance
+var observer = new MutationObserver(callback);
+
+// Start observing the document body for DOM mutations
+observer.observe(document.body, { childList: true, subtree: true });
