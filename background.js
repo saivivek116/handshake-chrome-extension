651fc138ff13cfd2438f38ec9491ea50f27e69d2
saivivek mac <saivivek116@gmail.com>
2024-04-06 19:28:24 -0400

    api integration changes

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "openTestWindow") {
         chrome.windows.create({
             url: 'test.html', // URL of the test page you want to open
-            type: 'normal',
+            type: 'panel',
             // width: 800,
             // height: 600
         });
--- test.js
+++ test.js
@@ -1,74 +1,22 @@
 
-let questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
-    {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
-  
+let questions = [];
 
-     {
-     numb: 6,
-     question: "Your Question is Here",
-  
-     options: [
-       "Option 1",
-       "option 2",
-       "option 3",
-       "option 4"
-    ]
-   },
-];
+document.addEventListener('DOMContentLoaded', () => {
+    //fetch the questions from the server
+    fetch('https://requestly.tech/api/mockv2/questions?teamId=2dwMVYYRZiPLoMuewEgl').then(response => response.json()).then(data => {
+        questions = data;
+        if(questions.length === 0){
+            throw new Error('No questions found');
+        }
+        info_box.classList.add("activeInfo");
+    }).catch(error => {
+        console.error('Error fetching questions', error);
+        let errorElement = document.querySelector('.error_box');
+        errorElement.classList.add('activeError');
+    });
+    console.log(questions);
+});
+
 
 let userResponses = [];
 //selecting all required elements
@@ -83,7 +31,6 @@ const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
-info_box.classList.add("activeInfo");
 
 // if exitQuiz button clicked
 exit_btn.onclick = ()=>{
@@ -114,24 +61,7 @@ let widthValue = 0;
 // const restart_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
 
-// if restartQuiz button clicked
-// restart_quiz.onclick = ()=>{
-//     quiz_box.classList.add("activeQuiz"); //show quiz box
-//     result_box.classList.remove("activeResult"); //hide result box
-//     timeValue = 15; 
-//     que_count = 0;
-//     que_numb = 1;
-//     userScore = 0;
-//     widthValue = 0;
-//     showQuetions(que_count); //calling showQestions function
-//     queCounter(que_numb); //passing que_numb value to queCounter
-//     clearInterval(counter); //clear counter
-//     clearInterval(counterLine); //clear counterLine
-//     startTimer(timeValue); //calling startTimer function
-//     startTimerLine(widthValue); //calling startTimerLine function
-//     timeText.textContent = "Time Left"; //change the text of timeText to Time Left
-//     next_btn.classList.remove("show"); //hide the next button
-// }
+
 
 // if quitQuiz button clicked
 quit_quiz.onclick = ()=>{
@@ -175,7 +105,7 @@ function showQuetions(index){
     const que_text = document.querySelector(".que_text");
 
     //creating a new span and div tag for question and option and passing the value using array index
-    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let que_tag = '<span>'+ `${index+1}` + ". " + questions[index].question +'</span>';
 
     let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
     + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
