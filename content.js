c39c0c4b48dbd870fb9c864982bcd3018438ef4a
Sai Kiran <saikirankudumula@gmail.com>
2024-04-10 16:05:18 -0400

    Clean up

--- content.js
+++ content.js
@@ -1,7 +1,4 @@
-// Import libraries if needed for making API calls (e.g., fetch)
-// import fetch from 'node-fetch'; // This might be needed in your environment
 function setupBoxClickListener() {
-  const apiUrl = "http://127.0.0.1:3000/job-genie";
   const boxContainer = document.querySelector(".box-container");
   if (boxContainer) {
     boxContainer.addEventListener("click", function (event) {
@@ -17,7 +14,6 @@ function setupBoxClickListener() {
                 chrome.runtime.lastError.message
               );
             } else {
-              // Handle response here, if needed
             }
           }
         );
@@ -26,72 +22,87 @@ function setupBoxClickListener() {
   }
 }
 
-function insertBanner() {
-  // Assuming your API endpoint to process keywords is at 'https://your-api.com/process-keywords'
+function insertShimmerLoader() {
+  const loaderHtml = `<div class="shimmer-loader"></div>`;
 
-  var bannerHtml = `
-    <div id="my-custom-banner">
-      <img src="${chrome.runtime.getURL(
-        "images/icon16.png"
-      )}" alt="Icon" class="banner-icon">
-      <span><strong>17 of 32 keywords</strong> are present in your resume</span>
-      <div class="progress-bar">
+  targetDiv = document.querySelector("div.style__application-flow-new___JIHOx");
+  if (targetDiv) {
+    targetDiv.insertAdjacentHTML("afterend", loaderHtml);
+  }
+}
+
+async function insertBanner() {
+  insertShimmerLoader();
+  try {
+    const apiUrl = "http://127.0.0.1:3000/get-job-matching-insights";
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    var bannerHtml = `
+      <div id="my-custom-banner">
+        <img src="${chrome.runtime.getURL(
+          "images/icon16.png"
+        )}" alt="Icon" class="banner-icon">
+        <span><strong>${data.MatchingSkills?.length} of ${
+      data.MatchingSkills?.length + data.SkillsNotInResume?.length
+    } keywords</strong> are present in your resume</span>
+        <div class="progress-bar">
         <div class="progress"></div>
-      </div>
-      <span class="resume-match">53% resume match</span>
-      <button class="close-btn">×</button>
-    </div>
+        </div>
+        <span class="resume-match">${parseInt(
+          data.MatchPercentage
+        )}% resume match</span>
+              </div>
       <div class="box-container">
         <div class="box">What are the core responsibilities of this job?</div>
         <div class="box">What is the typical career path for someone in this role?</div>
         <div class="box">What specific programming languages and technologies should a candidate be proficient in for this role?</div>
-        <div class="box">Could you elaborate on the kinds of projects a Junior Software Developer would be handling initially?</div>
+        <div class="box">Help me develop important leadership skills?</div>
       </div>
-  `;
+    `;
 
-  var targetDiv = document.querySelector(
-    "div.style__application-flow-new___JIHOx"
-  );
+    var targetDiv = document.querySelector(
+      "div.style__application-flow-new___JIHOx"
+    );
 
-  const button = document.createElement("button");
+    const button = document.createElement("button");
 
-  button.textContent = "Take Test";
+    button.textContent = "Take Test";
 
-  button.classList.add("take-test-button");
-  button.style.bottom = "20px";
-  button.style.right = "20px";
-  button.style.zIndex = "1000";
+    button.classList.add("take-test-button");
+    button.style.bottom = "20px";
+    button.style.right = "20px";
+    button.style.zIndex = "1000";
 
-  button.addEventListener("click", () => {
-    chrome.runtime.sendMessage({ action: "openTestWindow" });
-  });
+    button.addEventListener("click", () => {
+      chrome.runtime.sendMessage({ action: "openTestWindow" });
+    });
 
-  button.textContent = "Take Test";
-  button.classList.add("take-test-button");
-  button.style.bottom = "20px";
-  button.style.right = "20px";
-  button.style.zIndex = "1000";
-
-  button.addEventListener("click", () => {
-    chrome.runtime.sendMessage({ action: "openTestWindow" });
-  });
-
-  if (targetDiv) {
-    targetDiv.insertAdjacentHTML("afterend", bannerHtml);
-    var closeButton = document.querySelector("#my-custom-banner .close-btn");
-    if (closeButton) {
-      closeButton.addEventListener("click", function () {
-        document.querySelector("#my-custom-banner").remove();
-      });
+    if (targetDiv) {
+      targetDiv.insertAdjacentHTML("afterend", bannerHtml);
+      // var closeButton = document.querySelector("#my-custom-banner .close-btn");
+      // if (closeButton) {
+      //   closeButton.addEventListener("click", function () {
+      //     document.querySelector("#my-custom-banner").remove();
+      //   });
+      // }
+      targetDiv.appendChild(button);
+    } else {
+      console.log("Target div still not found after DOM changes.");
     }
-    targetDiv.appendChild(button);
-  } else {
-    console.log("Target div still not found after DOM changes.");
+  } catch (error) {
+    console.error("Failed to fetch banner details:", error);
+  } finally {
+    const loader = document.querySelector(".shimmer-loader");
+    if (loader) {
+      loader.remove();
+    }
   }
   setupBoxClickListener();
 }
 
-// MutationObserver callback to execute when mutations are observed
 var callback = function (mutationsList, observer) {
   for (var mutation of mutationsList) {
     if (mutation.type === "childList") {
--- popup.js
+++ popup.js
@@ -1,3 +1,34 @@
-document.getElementById('click-me').addEventListener('click', function() {
-    alert('Button clicked!');
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://127.0.0.1:3000/get-job-matching-insights")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("content").style.display = "block";
+      displayData(data);
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      );
+    });
 });
+
+function displayData(data) {
+  const skillsList = document.getElementById("skills-list");
+  skillsList.innerHTML = ""; // Clear existing list items
+  data.SkillsNotInResume.forEach((skill) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = skill;
+    skillsList.appendChild(listItem);
+  });
+
+  document.getElementById("resume-match").textContent = `${parseInt(
+    data.MatchPercentage
+  )}% Resume match`;
+}
